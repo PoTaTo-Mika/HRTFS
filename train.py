88bf53a3ef36958f724dbf55ea2429424fb101e7
PoTaTo <1228427403@qq.com>
2025-01-20 22:32:07 +0800

    Update SwinTransformer

---
+++ train.py
@@ -0,0 +1,25 @@
+from swin_transformer import SwinTransformerV2
+
+"""
+    Args:
+        input_shape (tuple): Shape of the input array. Default: (224, 224).
+        patch_size (int | tuple(int)): Patch size. Default: 4
+        in_chans (int): Number of input array channels. Default: 3
+        num_classes (int): Number of classes for classification head. Default: 1000
+        embed_dim (int): Patch embedding dimension. Default: 96
+        depths (tuple(int)): Depth of each Swin Transformer layer.
+        num_heads (tuple(int)): Number of attention heads in different layers.
+        window_size (int): Window size. Default: 7
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim. Default: 4
+        qkv_bias (bool): If True, add a learnable bias to query, key, value. Default: True
+        drop_rate (float): Dropout rate. Default: 0
+        attn_drop_rate (float): Attention dropout rate. Default: 0
+        drop_path_rate (float): Stochastic depth rate. Default: 0.1
+        norm_layer (nn.Module): Normalization layer. Default: nn.LayerNorm.
+        ape (bool): If True, add absolute position embedding to the patch embedding. Default: False
+        patch_norm (bool): If True, add normalization after patch embedding. Default: True
+        use_checkpoint (bool): Whether to use checkpointing to save memory. Default: False
+        pretrained_window_sizes (tuple(int)): Pretrained window sizes of each layer.
+"""
+
+model = SwinTransformerV2(input_shape=()
